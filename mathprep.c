951e6affecac79d6ed34f7a990a3dbd13c1f17eb
Jaakko Linnosaari <jaakko.linnosaari@iki.fi>
2011-10-17 21:06:06 +0300

    improved logic for selecting numbers
    It's dull if the same numbers appear in the same order in consecutive calculations so
    we'll make sure that won't happen.

--- mathprep.c
+++ mathprep.c
@@ -18,19 +18,29 @@ int random_int(int min, int max)
 
 int main(int argc, char *argv[])
 {
-	int x, y, result, answer;
+	int x, y, prev_x, prev_y, result, answer;
 
 	printf("mathprep\n\n");
 	init_randomness();
 
+	x = y = 0;
+
 	for(;;)
 	{
-		x = random_int(1,8);
-		y = random_int(1,3);
+		prev_x = x;
+		prev_y = y;
+
+		do
+		{
+			x = random_int(1,8);
+			y = random_int(1,3);
+		} while (x==prev_x && y==prev_y);
 		result = x + y;
+
 		printf("%i + %i = ",x,y);
 		if(!scanf("%i",&answer))
 			break;
+
 		printf( (result==answer) ? "Correct!\n\n" : "Wrong...\n\n" );
 	}
 	exit(0);
