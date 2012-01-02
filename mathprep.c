f78fa2b529024913a90fb1f01e9c6ceb372c9661
Jaakko Linnosaari <jaakko.linnosaari@iki.fi>
2012-01-02 14:21:38 +0200

    subtraction

--- mathprep.c
+++ mathprep.c
@@ -6,6 +6,9 @@
 
 #define BUFFSIZE 100
 
+#define OP_ADD 1
+#define OP_SUB 2
+
 void init_randomness()
 {
 	struct timeval t;
@@ -24,27 +27,42 @@ int random_int(int min, int max)
 
 int main(int argc, char *argv[])
 {
-	int x, y, prev_x, prev_y, result;
-	char *str;
+	int x, y, op, prev_x, prev_y, prev_op, result;
+	char operator, *str;
 
 	printf("mathprep\n\n");
 	init_randomness();
 
-	x = y = 0;
+	x = y = op = 0;
 	if ((str = malloc(BUFFSIZE)) == NULL)
 		exit(EXIT_FAILURE);
 
 	for (;;) {
 		prev_x = x;
 		prev_y = y;
+		prev_op = op;
 
 		do {
 			x = random_int(1, 8);
 			y = random_int(1, 3);
-		} while (x == prev_x && y == prev_y);
+			op = random_int(1, 2);
+		} while (x == prev_x && y == prev_y && op == prev_op);
 
-		snprintf(str, BUFFSIZE, "%i + %i", x, y);
-		result = x + y;
+		switch (op) {
+		case OP_ADD:
+			operator = '+';
+			result = x + y;
+			break;
+		case OP_SUB:
+			operator = '-';
+			result = x - y;
+			break;
+		default:
+			/* this should not happen :) */
+			assert(0);
+		};
+
+		snprintf(str, BUFFSIZE, "%i %c %i", x, operator, y);
 		if (!ask(str, result))
 			break;
 	}
